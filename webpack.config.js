3378dcd3eef42082aad586e05c0430da5603c728
Antony Budianto <antonybudianto@gmail.com>
2019-05-01 16:18:46 +0700

    setup webpack for client.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
 const path = require('path');
 
-module.exports = {
-  entry: './web/web.js',
-  output: {
-    filename: 'bundle.js',
-    publicPath: '/web/',
-    path: path.resolve(__dirname, 'web/dist')
+module.exports = [
+  {
+    entry: './web/web.js',
+    output: {
+      filename: 'bundle.js',
+      publicPath: '/web/',
+      path: path.resolve(__dirname, 'web/dist')
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      ]
+    }
   },
-  module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  {
+    entry: './public/client.js',
+    output: {
+      filename: 'client.min.js',
+      publicPath: '/public/',
+      path: path.resolve(__dirname, 'public')
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      ]
+    }
   }
-};
+];
