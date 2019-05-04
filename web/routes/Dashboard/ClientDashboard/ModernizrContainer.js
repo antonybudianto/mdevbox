efc81f35ee0748d1fe9876860d3b6843bb9493ba
Antony Budianto <antonybudianto@gmail.com>
2019-05-04 11:36:47 +0700

    enchance input types display

--- web/routes/Dashboard/ClientDashboard/ModernizrContainer.js
+++ web/routes/Dashboard/ClientDashboard/ModernizrContainer.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ModernizrStyle } from './ModernizrStyle';
 
+const renderInputTypes = (k, v) => {
+  const keys = Object.keys(v);
+  const vals = Object.values(v);
+  return keys.map((ck, i) => (
+    <li className={vals[i] ? 'green' : 'orange'} key={ck}>
+      {ck}
+    </li>
+  ));
+};
 const ModernizrContainer = ({ stat }) => {
   const keys = Object.keys(stat);
   const vals = Object.values(stat);
+
   return (
     <div className={ModernizrStyle}>
       <ul>
@@ -11,6 +21,9 @@ const ModernizrContainer = ({ stat }) => {
           return (
             <li className={!!vals[i] ? 'green' : 'orange'} key={k}>
               {k}
+              {['input', 'inputtypes'].indexOf(k) !== -1 && (
+                <ul>{renderInputTypes(k, vals[i])}</ul>
+              )}
             </li>
           );
         })}
